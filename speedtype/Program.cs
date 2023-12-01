c16dae0c11c836a459534b919286e40e911320a5
david <zollner.david010306@gmail.com>
2023-12-01 16:22:53 +0100

    read files and user input

--- speedtype/Program.cs
+++ speedtype/Program.cs
@@ -9,6 +9,19 @@ public class Program
             Console.WriteLine("Please enter a valid Path to an input file");
             return;
         }
+        fileLocation = args[0];
+
+        string[] content = File.ReadAllLines(fileLocation);
+        foreach (string word in content)
+        {
+            Console.WriteLine($"{word} ");
+            for (int i = 0; i < word.Length; ++i)
+            {
+                char input = Console.ReadKey().KeyChar;
+            }
+            Console.Clear();
+        }
+        
         Console.WriteLine("Hello World");
     }
 }
