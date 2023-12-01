39e5a5066822af64a4597d6f686182a4c3552a07
david <zollner.david010306@gmail.com>
2023-12-01 16:06:21 +0100

    Build file structure and create source file

---
+++ speedtype/Program.cs
@@ -0,0 +1,14 @@
+﻿
+public class Program
+{
+    private static string fileLocation = String.Empty;
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Please enter a valid Path to an input file");
+            return;
+        }
+        Console.WriteLine("Hello World");
+    }
+}
